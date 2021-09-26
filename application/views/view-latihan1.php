73b05c9fbb0d8e4746560d06c1bb49510c59d0ba
Msyrfh <musyarofahhani2@gmail.com>
2021-09-26 15:29:32 +0700

    Menambah file file yang sudah dibuat

---
+++ application/views/view-latihan1.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    Halo kawan..yuk kita belajar web programming..!!<br>
+    Nilai ke 1 adalah <?= $nilai1; ?>
+    , dan nilai ke 2 adalah <?= $nilai2; ?> <br>
+    ini hasil dari pemodelan dengan metode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 .
+                                                                    " = " . $hasil; ?>
+</body>
+
+</html>
